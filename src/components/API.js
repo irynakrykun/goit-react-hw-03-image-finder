d0be62d714331d19fab8fd553b15ff6c64ce09e4
irynakrykun <ira_franyk@ukr.net>
2023-01-20 20:13:10 +0200

    test

--- src/components/API.js
+++ src/components/API.js
@@ -1,8 +1,33 @@
+// const BASE_URL = 'https://pixabay.com/api/';
+// const KEY = '31580624-e5f87d112d57e9afad49661fb';
+// function fetchImage(name = 'cat') {
+//   return fetch(
+//     `https://pixabay.com/api/?q=city&page=1&key=31580624-e5f87d112d57e9afad49661fb&image_type=photo&orientation=horizontal&per_page=12`
+//   )
+//     .then(response => {
+//      console.log(response);
+
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+      
+//     })
+//     .then(data => {console.log(data);
+//       ;
+//     })
+//     .catch(error => {
+//       // Error handling
+//     });
+// }
+// export default{fetchImage}
+
+ 
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '31580624-e5f87d112d57e9afad49661fb';
-function fetchImage(name = 'cat') {
-  fetch(
-    `${BASE_URL}?q={name}&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+function fetchImage(name = cat) {
+  return fetch(
+    `https://pixabay.com/api/?q=city&page=1&key=31580624-e5f87d112d57e9afad49661fb&image_type=photo&orientation=horizontal&per_page=12`
   )
       .then(response => {
         console.log(response);
@@ -11,14 +36,11 @@ function fetchImage(name = 'cat') {
       }
       return response.json();
     })
-    // .then(data => {console.log(response.data.hits);
-    //   ;
-    // })
-    // .catch(error => {
-    //   // Error handling
-    // });
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
-export default{fetchImage}
-
-   
-
+fetchImage()
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,38 @@
-import { Component } from 'react';
-import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
-import Searchbar from './Searchbar/Searchbar';
-import fetchImage from './API';
+// import { Component } from 'react';
+// import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+// import Searchbar from './Searchbar/Searchbar';
+// import fetchImage from './API';
 
-export default class App extends Component {
-  state = {
-    tagName: '',
-  };
+// export default class App extends Component {
+//   state = {
+//     tagName: '',
+//   };
 
-  handelFormSubmit = tagName => {
-    this.setState({ tagName });
-  };
+//   handelFormSubmit = tagName => {
+//     this.setState({ tagName });
+//   };
 
-  componentDidMount() {
-    fetchImage().then(data => {
-      console.log(data);
-    });
-  }
+//   componentDidMount() {
+//     fetchImage().then(data => {
+//       console.log(response.data.hits);
+//     });
+//   }
 
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Searchbar onSubmit={this.handelFormSubmit} />
-        <ImageGalleryItem tagName={this.setState.tagName} />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div
+//         style={{
+//           height: '100vh',
+//           display: 'flex',
+//           justifyContent: 'center',
+//           alignItems: 'center',
+//           fontSize: 40,
+//           color: '#010101',
+//         }}
+//       >
+//         <Searchbar onSubmit={this.handelFormSubmit} />
+//         <ImageGalleryItem tagName={this.setState.tagName} />
+//       </div>
+//     );
+//   }
+// }
